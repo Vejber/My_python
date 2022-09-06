f38650fdff875ef93accfb6b67b062987c7fee97
Vejber <105496392+Vejber@users.noreply.github.com>
2022-09-06 16:30:39 +0400

    sum of nums

---
+++ example1.py
@@ -0,0 +1,5 @@
+number_1 = int(input("Введите первое число"))
+number_2 = int(input("Введите второе число"))
+number_3 = int(input("Введите третье число"))
+result = number_1 + number_2 + number_3
+print(result)
