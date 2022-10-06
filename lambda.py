cad2af61a081096731ff4938128b96dd90a091ac
Vejber <105496392+Vejber@users.noreply.github.com>
2022-10-06 14:40:37 +0400

    lambda

---
+++ lambda.py
@@ -0,0 +1,26 @@
+# function = argument of another function
+# def calc1(x):
+#     return x+10
+
+
+# def calc2(x):
+#     return x*10
+
+
+# def math(op, x):
+#     print(op(x))
+
+
+# math(calc1, 10)
+# math(calc2, 10)
+
+# lambda:
+# def f(x, y): return x+y
+
+
+# further:
+def math(op, x):
+    print(op(x))
+
+
+math(lambda x: x*10, 10)
