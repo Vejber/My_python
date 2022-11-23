4ac8a7c19801de583ed502a9ed770de6b3586524
Vejber <105496392+Vejber@users.noreply.github.com>
2022-11-23 15:15:08 +0400

    jupyter

---
+++ HW/HW10/task1.py
@@ -0,0 +1,20 @@
+# f(x) = 18x^3+5x^2 + 10x - 30
+
+# Определить корни
+
+# import sympy
+# x = sympy.Symbol('x')
+# f = 18*x**3+5*x**2+10*x-30
+# sympy.solve(f, x)
+
+# Построить график
+# sympy.plot(f)
+
+# Вычислить вершину
+# sympy.solve(sympy.diff(f,x),x)
+
+# Определить промежутки, на котором f > 0
+# sympy.solve(f>0,x)
+
+# Определить промежутки, на котором f < 0
+# sympy.solve(f<0,x)
